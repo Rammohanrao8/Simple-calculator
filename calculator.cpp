95d363c27d0eab8d53b1b232aca824f1f2fa689d
Ponnamanda Ram Mohan Rao <Ponnamanda41@gmail.com>
2025-02-12 11:31:42 +0530

    initial commit

---
+++ calculator.cpp
@@ -0,0 +1,47 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    cout << "**********CALCULATOR**********\n";
+
+    char op;
+    double num1;
+    double num2;
+    double result;
+
+    cout << "Enter the Operator (+ - * /) : ";
+    cin >> op;
+
+    cout << "Enter the First number : ";
+    cin >> num1;
+
+    cout << "Enter the Second number :";
+    cin >> num2;
+
+    switch(op){
+
+        case '+':
+            result = num1 + num2;
+            cout << "Result : " << result << endl;
+            break;
+        case '-':
+            result = num1 - num2;
+            cout << "result : " << result << endl;
+            break;
+        case '*':
+            result = num1 * num2;
+            cout << "Result : " << result << endl;
+            break;
+        case '/':
+            result = num1 / num2;
+            cout << "result : " << result << endl;
+            break;
+        default:
+            cout <<"Please enter the valid operators and numbers..!" << endl;
+            break;
+    }
+
+
+    cout << "******************************";
+    return 0;
+}
